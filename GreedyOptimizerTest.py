e321097d10a90c58927d28ff86d2a89e6f44fe38
Xwyzworms <pratamaazmi1@gmail.com>
2022-12-20 16:39:14 +0700

    init

---
+++ GreedyOptimizerTest.py
@@ -0,0 +1,31 @@
+
+
+import unittest
+import change
+
+class TestChange(unittest.TestCase):
+
+    def test_get_quarter(self):
+        ...
+    
+    def test_get_dime(self):
+        ...
+
+    def test_get_nickel(self):
+        ...
+    
+    def  test_get_penny(self):
+        ...
+    
+    def test_small_number(self):
+        ...
+    
+    def test_large_number(self):
+        ...
+    
+    def test_get_quarter_dime_penny(self):
+        ...
+    
+    def test_get_quarter_dime_nickle_penny(self):
+        ...
+    
